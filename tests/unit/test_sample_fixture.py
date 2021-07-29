1806fb5288888ccba1e3444a95c2c15f3d206e1a
Behzad Sezari <b.sezari.1991@gmail.com>
2021-07-29 18:53:34 +0200

    add test directory tree (unit-test, integration-test, fixtures )

---
+++ tests/unit/test_sample_fixture.py
@@ -0,0 +1,37 @@
+#%%
+from tests.integration.fixtures.people_data import sample_people_data
+# from ..conftest import *
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from src.main import format_data_for_display
+#%%
+print(sys.path)
+def test_input():
+    string = "behzad"
+    assert isinstance(string, str)
+        
+
+
+
+def test_format_data_for_display(sample_people_data):
+    assert format_data_for_display(sample_people_data) == [
+        "Alfonsa Ruiz: Senior Software Engineer",
+        "Sayid Khan: Project Manager",
+    ]
+
+
+# format_data_for_display([
+#         {
+#             "given_name": "Alfonsa",
+#             "family_name": "Ruiz",
+#             "title": "Senior Software Engineer",
+#         },
+#         {
+#             "given_name": "Sayid",
+#             "family_name": "Khan",
+#             "title": "Project Manager",
+#         },
+#     ])
+# %%
