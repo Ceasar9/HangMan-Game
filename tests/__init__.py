52cecf2575d74bb1137bda5342d8a9ac720e7341
Behzad Sezari <16059626+Ceasar9@users.noreply.github.com>
2021-08-08 16:56:01 +0200

    fix test bug

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,6 +2,6 @@ import sys
 from os.path import dirname, abspath, join, sep
 package = dirname(dirname(abspath(__file__)))
 # print(mopy.split(sep)[-1].lower(), "***")
-assert package.split(sep)[-1].lower() == '1_print_debug_strmanipulation'
+# assert package.split(sep)[-1].lower() == '1_print_debug_strmanipulation'
 sys.path.append(package)
 print('package folder appended to path: ', package)
